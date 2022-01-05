8f00f0287eb7fcb9d7f5c8bd392a51c7d3d7bf73
Dementev_Artem <artemdementev561@gmail.com>
2022-01-05 21:17:39 +0200

    add file to gitHub

---
+++ Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace Factorial
+{
+    class Program
+    {
+        static BigInteger Factorial(int N) =>  N == 0 ? 1 : N * Factorial(N - 1);
+        
+        static void Main(string[] args)
+        {
+            var N = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("N = " + N);
+                    Console.WriteLine("N = " + Factorial(N));
+                    N++;
+                    Thread.Sleep(10);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    break;
+                }
+            }
+        }
+    }
+}
